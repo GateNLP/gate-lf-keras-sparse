42fce050c0844a8b2aaf15ab80ab69ab31ab8189
Johann Petrak <johann.petrak@gmail.com>
2016-08-16 15:12:53 +0200

    Include the method for suppressing stderr/out here too for testing.

--- testing/test_apply_m1.py
+++ testing/test_apply_m1.py
@@ -1,6 +1,13 @@
 from __future__ import print_function
 import numpy as np
+import sys
+# f = open('/dev/null','w')
+#oout=sys.stdout
+#oerr=sys.stderr
+#sys.stdout=f
+#sys.stderr=f
 from keras.models import load_model
+#sys.stderr=oerr
 
 X=np.loadtxt("m1/indep.csv",delimiter=",")
 y=np.loadtxt("m1/dep.csv",delimiter=",")
@@ -13,5 +20,5 @@ probs=model.predict_proba(X[0:10],verbose=False)
 ## print("true labels: ",y[0:10])
 ## print("predictions: ",probs[0:10])
 
-
+print("finish!",file=sys.stderr)
 
